a3b632c6400405245ab54524b9c68a824862b84a
sangam <applem3@Apples-MacBook-Air-2.local>
2025-07-11 08:09:33 +0545

    hellow

---
+++ code.java
@@ -0,0 +1,11 @@
+public class Code {
+    public static void main(String[] args) {
+        // Generate and print 10 random integers between 1 and 100
+        java.util.Random rand = new java.util.Random();
+        System.out.println("Random numbers:");
+        for (int i = 0; i < 10; i++) {
+            int randomNum = rand.nextInt(100) + 1;
+            System.out.println(randomNum);
+        }
+    }
+}
